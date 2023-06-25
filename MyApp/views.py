dab3bf7434359ce6bc94fa716f29bda32ba1ff4a
Sharath RC <sharathr.sh@gmail.com>
2023-06-25 16:40:53 +0200

    update with new project

--- MyApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Article
-# Create your views here.
-
-
-def index(request):
-    articles = Article.objects.all()
-
-    return render(request, 'index.html', {'articles':articles})
---
+++ MyProject/MyApp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Article
+
+
+def index(request):
+    articles = Article.objects.all()
+    
+    return render(request, 'index.html', {'article':articles})
+    
+    
